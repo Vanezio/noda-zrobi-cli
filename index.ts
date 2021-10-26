63930ef5db465dc0ac16309a2a771411196b7ead
ibohatov <i.bohatov@air.io>
2021-10-26 17:07:36 +0300

    working allright but doesnt shoose correctly build files

--- index.ts
+++ index.ts
@@ -1,14 +1,58 @@
 #!/usr/bin/env node
-const path = require('path');
-const fs = require('fs');
+import path from "path"
+import fs from "fs"
 
-const argv = process.argv[2]
+const argv = process.argv[2].toLocaleLowerCase();
+
+if (argv > "" && /^[a-zA-Z]+$/.test(argv)) {
 
 const capArgv = argv.charAt(0).toUpperCase() + argv.slice(1);
 
 const crudArr = ["get", "post", "put", "patch", "delete"];
 
 fs.promises.mkdir(path.join(process.cwd(), 'src', 'api', argv));
+
+let indexRes = "";
+
+fs.readFile(path.join(process.cwd(), 'src', 'api', "index.ts"), function(err: NodeJS.ErrnoException | null, data: object) {
+  if(err) throw err;
+
+  let lastImport = 0;
+  let lastRoute = 0;
+
+  const arr = data.toString().replace(/\r\n/g,'\n').split('\n');
+
+  arr.forEach((elem: string, index: number) => {
+    if(elem.includes("import")) {
+      lastImport = index
+    }
+  })
+
+  arr.splice(lastImport + 1, 0, `import ${argv + "Router"} from './${argv}';`);
+
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i].includes("registerRouters")) {
+      lastRoute = 1
+    } else if (lastRoute === 1 && arr[i].includes("}")){
+      lastRoute = i;
+      i = arr.length
+    }
+  }
+
+  if(lastRoute > 0) {
+  arr.splice(lastRoute, 0, `app.use('/${argv}s', ${argv}Router);`);
+  }
+
+  indexRes = arr.reduce((prev, curr) => {
+    return prev + "\n" + curr
+  })
+
+  if(indexRes.includes(argv)) {
+    fs.promises.writeFile(path.join(process.cwd(), 'src', 'api', "index.ts"), indexRes)
+  }
+});
+
+
 
 crudArr.forEach((elem) => {
     fs.promises.writeFile(path.join(process.cwd(), 'src', 'api', argv, elem + ".ts"),
@@ -41,4 +85,10 @@ export default router;
 `
 );
 
+
+} else {
+  console.log(
+    `ERROR
+    Argument is empty or argument should contain only letters`)
+}
 
